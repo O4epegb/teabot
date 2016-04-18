28261f34bbb74b481d405f88c8631fc2c416f1cf
Danila <O4epegb@mail.ru>
2016-04-18 23:33:16 +0400

    added html index page

--- web.js
+++ web.js
@@ -5,20 +5,22 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.json({ version: packageInfo.version });
+// app.get('/', function(req, res) {
+//     res.json({version: packageInfo.version});
+// });
+
+app.use(express.static('public'));
+
+var server = app.listen(process.env.PORT, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Web server started at http://%s:%s', host, port);
 });
 
-var server = app.listen(process.env.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Web server started at http://%s:%s', host, port);
-});
-
-module.exports = function (bot) {
-  app.post('/' + bot.token, function (req, res) {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
-  });
+module.exports = function(bot) {
+    app.post('/' + bot.token, function(req, res) {
+        bot.processUpdate(req.body);
+        res.sendStatus(200);
+    });
 };
